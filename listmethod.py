60f5058c3a17438c99925f00bf8530e5b8d061ab
sudarsanjilu <sudarsan.devops93@gmail.com>
2024-11-03 14:06:39 +0530

    listmethod

---
+++ listmethod.py
@@ -0,0 +1,21 @@
+colors = ["voilet", "indigo", "blue", "green"]
+colors.sort()
+print(colors)
+colors.reverse()
+print(colors)
+
+colors.sort(reverse=True)
+
+
+#number
+num = [4,2,5,3,6,1,2,1,2,8,9,7]
+num.sort()
+print(num)
+num.sort(reverse=True)
+print(num)
+
+#copy 
+colors = ["voilet", "green", "indigo", "blue"]
+newlist = colors.copy()
+print(colors)
+print(newlist)
