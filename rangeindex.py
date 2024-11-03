e80d3b3eb60ce81b3e17971ca74701fd7722f5e4
sudarsanjilu <sudarsan.devops93@gmail.com>
2024-11-03 13:16:45 +0530

    range index

---
+++ rangeindex.py
@@ -0,0 +1,7 @@
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[3:7])	#using positive indexes
+print(animals[-7:-2])	#using negative indexes
+print(animals[:])
+print(animals[1:])
+print(animals[1:2])
+print(animals[1:1:2])
