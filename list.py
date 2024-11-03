36d1b2a1bdd859d3f8574b97514e9fa10a569cb5
sudarsanjilu <sudarsan.devops93@gmail.com>
2024-11-03 12:55:13 +0530

    list file

--- list.py
+++ list.py
@@ -2,3 +2,12 @@ lst1 = [1,2,2,3,5,4,6]
 lst2 = ["Red", "Green", "Blue"]
 print(lst1)
 print(lst2)
+
+colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+#          [-5]    [-4]    [-3]     [-2]      [-1]
+
+colors2 = len(colors)
+print("len of", len(colors))
+print(colors[-1])
+print(colors[-3])
+print(colors[-5])
