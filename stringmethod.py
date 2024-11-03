7dc449a56568e24d45be82b2ad9bebc293589563
sudarsanjilu <sudarsan.devops93@gmail.com>
2024-11-03 12:34:53 +0530

    string method file

---
+++ stringmethod.py
@@ -0,0 +1,11 @@
+str1 = "sudarsan is very good boy"
+str1.count(str1)
+str2= str1.__le__(str1)
+print(str1)
+print(str2)
+#str1.endswith(boy)
+
+str4 = "Captain"
+str5 = "America"
+str6 = str4 + " " + str5
+print(str6)
